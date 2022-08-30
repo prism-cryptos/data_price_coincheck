79b0d9032987ae1a5d01188a6c2c2086e670cd41
prism <prismeconomy.contact@gmail.com>
2022-08-30 20:47:57 +0900

    create a parameter to select target coin

--- coincheck_daily_closing_price_in_all_years.py
+++ coincheck_daily_closing_price_in_all_years.py
@@ -6,68 +6,65 @@ import pandas as pd
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--disable-extensions')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--disable-gpu')
 options.add_argument('--blink-settings=imagesEnabled=false')
 options.add_argument('--no-sandbox')
 options.add_argument('--log-level=3')
 options.page_load_strategy = 'eager' #https://www.selenium.dev/ja/documentation/webdriver/capabilities/shared/
 
-all_year_data = pd.DataFrame()
 
-for y in range(9): #When I wrote this script, 9 is a good parameter(the number of showed years on the website).
+def scrape_closing_price(coin_param): 
+    all_year_data = pd.DataFrame()
 
-    for m in range (12):
+    for y in range(9): #When I wrote this script, 9 is a good parameter(the number of showed years(2014~2022) on the Coincheck website, which is 9) to get BTC price.
 
-        time.sleep(5)
-        driver = webdriver.Chrome('./chromedriver.exe', options=options)
-        driver.implicitly_wait(10)
-        driver.get("https://coincheck.com/ja/exchange/closing_prices")
+        for m in range (12):
 
-        #select year pulldown option and get label text which correspond to value
-        y_pd = driver.find_element_by_xpath("//div[@class = 'table-top']/div/select[@class = 'year ng-pristine ng-untouched ng-valid']")
-        y_pd_select = Select(y_pd)
-        y_pd_select.select_by_value(str(y))
+            time.sleep(1)
+            driver = webdriver.Chrome('./chromedriver.exe', options=options)
+            driver.implicitly_wait(5)
+            driver.get("https://coincheck.com/ja/exchange/closing_prices")
 
-        option_element = driver.find_element_by_xpath("//div[@class = 'table-top']/div/select/option[@value = '" + str(y) + "']")
-        label_year = option_element.text
+            #select year pulldown option and get label text which correspond to value
+            y_pd = driver.find_element_by_xpath("//div[@class = 'table-top']/div/select[@class = 'year ng-pristine ng-untouched ng-valid']")
+            y_pd_select = Select(y_pd)
+            y_pd_select.select_by_value(str(y))
+            time.sleep(2)
 
-        #select month pulldown option
-        m_pd = driver.find_element_by_xpath("//div[@class = 'table-top']/div/select[@class = 'month ng-pristine ng-untouched ng-valid']")
-        m_pd_select = Select(m_pd)
-        m_pd_select.select_by_value(str(m))
+            option_element = driver.find_element_by_xpath("//div[@class = 'table-top']/div/select/option[@value = '" + str(y+3) + "']")
+            label_year = option_element.text
 
-        #get date values
-        date_values = driver.find_elements_by_xpath("//tbody/tr/th[@class = 'fixed ng-binding']")
-        date = [str(label_year) + "/" + s.text for s in date_values]   #https://qiita.com/waterada/items/2aa4aa14140a8d0542f3
-        date = pd.DataFrame(date) 
-        
-        #get price values
-        price_values = driver.find_elements_by_xpath("//tbody/tr/td[@class = 'ng-binding ng-scope']")
-        prices = [s.text for s in price_values]
+            #select month pulldown option
+            m_pd = driver.find_element_by_xpath("//div[@class = 'table-top']/div/select[@class = 'month ng-pristine ng-untouched ng-valid']")
+            m_pd_select = Select(m_pd)
+            m_pd_select.select_by_value(str(m))
+            time.sleep(2)
 
-        #retreive only btc price(needs adjustment later)
-        BTC_price = []
-        for n in range(date.size):
-            if n == 0:   #0 means upper left-hand corner of the price data. So I choose BTC price by specifing 0 as a parameter.
-                BTC_price.append(prices[n])   
-            else:
-                hoge = n * 23   #This parameter 23 is depend on the number of cryptocurrencies handled by Coincheck. 
-                BTC_price.append(prices[hoge])
-        BTC_price = pd.DataFrame(BTC_price)
-        print(BTC_price)
+            #get date values
+            date_values = driver.find_elements_by_xpath("//tbody/tr/th[@class = 'fixed ng-binding']")
+            date = [str(label_year) + "/" + s.text for s in date_values]   #https://qiita.com/waterada/items/2aa4aa14140a8d0542f3
+            date = pd.DataFrame(date) 
 
-        for_append = pd.DataFrame(pd.concat([date, BTC_price], axis=1, ignore_index = True))
-        all_year_data = pd.DataFrame(pd.concat([all_year_data, for_append], axis=0, ignore_index = True))
-        driver.quit()
+            #get price values
+            price_values = driver.find_elements_by_xpath("//tbody/tr/td[@class = 'ng-binding ng-scope']")
+            prices = [s.text for s in price_values]
+
+            #retreive only btc price(needs adjustment later)
+            closing_price = []
+            for n in range(date.size): 
+                
+                hoge = (n * 23) + coin_param
+                closing_price.append(prices[hoge])
+                    
+            closing_price = pd.DataFrame(closing_price)
+            print(closing_price)
+
+            for_append = pd.DataFrame(pd.concat([date, closing_price], axis=1, ignore_index = True))
+            all_year_data = pd.DataFrame(pd.concat([all_year_data, for_append], axis=0, ignore_index = True))
+            driver.quit()
+
+    all_year_data.to_csv("coincheck_daily_closing_price.csv")
 
 
-
-all_year_data.to_csv("coincheck_daily_closing_price.csv")
-
-
-#If you do web scraping against an dynamic web page(created by Ajax), you can't use beautifulSoup only do Selenium.
-#Description: https://teratail.com/questions/375725
-
-#how to solve MaxRetryError from Selenium
-#https://qiita.com/S8s8Max/items/035742cf061419d827c6
+scrape_closing_price(3)
+# please set parameter following the information below. 
+#0--BTC, 1--ETH, 2--ETC, 3--LSK, 4--XRP, 5--XEM, 6--LTC, 7--BCH, ... 
